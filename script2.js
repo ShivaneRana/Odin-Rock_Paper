d16d989742148d8a7c879532294eaaf619fad22c
Shivane Rana <shivanerana2004@gmail.com>
2024-05-22 19:48:26 +0530

    script2 cleaned for better readability

--- script2.js
+++ script2.js
@@ -1,5 +1,5 @@
 //this array will be used to display rock paper and scissor over and over again
-const rockpaperscissor = [null,"Rock","Paper","Scissor"];
+const rockpaperscissor = [null,"stone","Paper","Scissor"];
 
 // userscore and computerscore are 0 at start of the game
 let userScore = 0;
@@ -7,37 +7,39 @@ let computerScore = 0;
 let userChoice = null;
 
 //creating new variables
-let rock = document.createElement("button");
+let stone = document.createElement("button");
 let paper = document.createElement("button");
 let scissor = document.createElement("button");
 const textArea = document.querySelector("#textArea");
 const buttonArea = document.querySelector("#buttonArea");
 const startGameButton = document.querySelector("#startGameButton");
+const roundNumber = document.querySelector("#roundNumber");
+const computerScoreArea = document.querySelector("#computerScoreArea");
+const userScoreArea = document.querySelector("#userScoreArea");
+
 
 //rock paper scissor button functionality
-rock.textContent = "Rock";
+stone.textContent = "Stone";
 paper.textContent = "Paper";
 scissor.textContent = "Scissor";
 
 
 //first phase of the game
 //as a side note coming up with this block of code took an entire day! :(
-//hopefully the code will work now just fine    
-startGameButton.addEventListener("click",() =>{
+//hopefully the code will work now just fine
+function gameStart1(){
+    startGameButton.addEventListener("click",() =>{
     textArea.textContent = "uwu1";
+    computerScoreArea.textContent = "Computer Score area shown here";
+    userScoreArea.textContent = "user score are shown here";
     startGameButton.textContent = "continue";
     startGameButton.addEventListener("click",() => {
         textArea.textContent = "uwu2";
+        computerScoreArea.textContent  = "Computer score: "+computerScore;
+        userScoreArea.textContent = "Player Score: "+userScore;
         startGameButton.textContent = "Enter the game";
-        startGameButton.addEventListener("click",playRound);
+        startGameButton.addEventListener("click",getComputerChoice);
     })
-})
+})};
 
-//this will play the game exactly one time
-function playRound(){
-    textArea.textContent = "the game will start from here";
-    startGameButton.remove();
-    buttonArea.append(rock)
-    buttonArea.append(paper)
-    buttonArea.append(scissor);
-}
+gameStart1();
